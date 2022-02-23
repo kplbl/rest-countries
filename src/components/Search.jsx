a578c567a68bbad7ee518e448be48d7b6fe86739
kplbl <51096422+kplbl@users.noreply.github.com>
2022-02-23 20:29:49 +0100

    connect state to input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import useStore from "../useStore";
 
 function Search() {
+  const filter = useStore((state) => state.filter);
+  const search = useStore((state) => state.search);
+  const setSearch = useStore((state) => state.setSearch);
+  const setFilter = useStore((state) => state.setFilter);
+
+  const onSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const onFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
   return (
     <div className="search">
       <div className="search__container shadow">
@@ -11,14 +25,20 @@ function Search() {
           className="search__input"
           placeholder="Search for a country..."
           type="text"
+          onChange={onSearch}
+          value={search}
         />{" "}
       </div>
 
-      <select className="search__select">
+      <select onChange={onFilter} value={filter} className="search__select">
         <option value="" disabled selected>
           Filter by region
         </option>
-        <option value="test">`test`</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">Americas</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
       </select>
     </div>
   );
