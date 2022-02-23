f20814b5b0dd4eec8fe439476fbf15d58f8487a4
kplbl <51096422+kplbl@users.noreply.github.com>
2022-02-23 20:29:32 +0100

    specify state

--- src/useStore.jsx
+++ src/useStore.jsx
@@ -2,6 +2,14 @@ import create from "zustand";
 
 const useStore = create((set) => ({
   dark: false,
+  countries: [],
+  search: "",
+  filter: "",
+  setCountries: (countries) => {
+    set({ countries });
+  },
+  setSearch: (value) => set({ search: value }),
+  setFilter: (value) => set({ filter: value }),
   toggle: () => set((state) => ({ dark: !state.dark })),
 }));
 
